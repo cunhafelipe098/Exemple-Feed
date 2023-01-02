44bed78034669af58c557a2392c71ff8f56c786b
dev <cunhafelipe098@gmail.com>
2023-01-02 17:13:46 -0300

    feature: update componet  post

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,38 @@ import './global.css'
 import styles from './App.module.css'
 import { Sidebar } from './components/Sidebar'
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/cunhafelipe098.png',
+      name: 'Felipe Cunha',
+      role: 'CTO D. Industry' 
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala Galera 👊'},
+      {type: 'paragraph', content: 'Acabei de subir mais um conteúdo no meu portifólio'},
+      {type: 'link', content: 'felipe.com.br'},
+    ],
+    publishedAt: new Date('2022-02-03 20:00:00'),
+  },
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat' 
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala Galera 👊'},
+      {type: 'paragraph', content: 'Acabei de subir mais um conteúdo no meu portifólio'},
+      {type: 'link', content: 'felipe.com.br'},
+    ],
+    publishedAt: new Date('2022-02-15 20:00:00'),
+  }
+]
+
+
 function App() {
 
   return (
@@ -15,14 +47,17 @@ function App() {
         <Sidebar />
 
         <main>
-          <Post 
-          author="Felipe" 
-          content="Web Develop"
-          /> 
-          <Post 
-            author="Ed Matheus" 
-            content="Web Develop"
-          />
+          {
+            posts.map(post => {
+              return (
+                <Post
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              )
+            })
+          }
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,45 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBr from 'date-fns/locale/pt-BR'
+
 import { Comment } from './Comment'
 import { Avatar } from './Avatar'
 import styles from './Post.module.css'
 
-export function Post(props) {
+export function Post({author, publishedAt, content}) {
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBr
+    })
+
+    const publisheddateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBr,
+        addSuffix: true
+    });
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/diego3g.png" />
+                    <Avatar src={author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>{props.author}</strong>
-                        <span>{props.content}</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>  
                 </div>
 
-                <time title="8 de maio" dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString() }>{publisheddateRelativeToNow}</time>
             </header>
 
             <div className={styles.content} >
-                <p>Fala Galera</p>
-                <p>Acabei de subir mais um conteúdo no meu portifólio</p>
-                <p><a href="">felipe.com</a></p>
-                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
-                
+                {content.map(line => {
+                    if(line.type == 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type == 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
             </div>
 
+         
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
